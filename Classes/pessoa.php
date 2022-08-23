c22bf14aa9e041656cfbd59660da1fcfce41bf36
Yan-Sena <25ygss@gmail.com>
2022-08-22 21:26:02 -0300

    Atividade Aula 22/08 PHP - Classes

---
+++ Classes/pessoa.php
@@ -0,0 +1,47 @@
+<?php 
+
+    class Pessoa 
+    {
+        var $codigo;
+        var $nome;
+        var $altura;
+        var $idade;
+        var $nascimento;
+        var $escolaridade;
+        var $salario;
+
+    
+        //aumenta a altura em centimetros
+    function crescer($centimetros)
+    { 
+        if ($centimetros > 0) {
+            $this->altura += $centimetros;
+        }
+    }
+
+    function formar($titulacao){
+
+        $this->escolaridade = $titulacao;
+
+    }
+        //aumenta a idade em anos
+
+    function envelhecer(){
+
+        if ($anos > 0) {
+            $this->idade += $anos;
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+?>
---
+++ exibeConta.php
@@ -0,0 +1,25 @@
+<?php 
+
+//insere a classe
+include_once 'Classes/conta.php';
+
+
+//Cria um Objeto
+    $contaX = new Contas;
+    
+    $contaX->numAge = 3301;
+    $contaX->numConta = 40028922;
+    $contaX->nome = "Astrogildo Ferreira";
+    $contaX->saldo = 100;
+
+
+
+
+//Imprimindo Informações na etiqueta
+
+/*$contaX->ExibeDados();*/
+
+
+
+
+?>
