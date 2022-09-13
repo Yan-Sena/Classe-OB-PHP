1d65fe6fbbe37705902e96fe43a15bfc66bd49f6
Yan-Sena <25ygss@gmail.com>
2022-09-12 22:02:22 -0300

    Aula(09_12) - 21%

--- Aula(09_12)/Classes/veiculorodoviario.php
+++ Aula(09_12)/Classes/veiculorodoviario.php
@@ -14,30 +14,80 @@
         }
 
 
-
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/ 
        protected function setanrodas($quant){
+
+        if(is_numeric($quant)&&($quant > 0)){
 
         $this->nrodas = $quant;
 
+        }else{
+            echo "Escreva um numero valido.";
+        }
+
        } //Usuario insere uma quantidade e a mesma é atribuida ao $nrodas
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/ 
 
 
        protected function pegarodas(){
 
-        
+        if(is_numeric($this->nrodas)&&($this->nrodas > 0)){
 
-        return $this->nrodas;
+            return $this->nrodas;
+
+        }else{
+
+            echo "Não há oque retornar ou é invalido";
+
+        }
 
        }  //INT
 
-
-       protected function setanpassageiros(){} 
-
-
-       protected function pegapassageiros(){} //INT
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/ 
 
 
-       public function veiculorodoviario(){}
+       protected function setanpassageiros($quant){
+
+        if(is_numeric($quant)&&($quant > 0)){
+
+            $this->npassageiro = $quant;
+    
+            }else{
+                echo "Escreva um numero valido.";
+            }
+
+       } 
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/ 
+
+
+       protected function pegapassageiros(){
+
+        if(is_numeric($this->npassageiro)&&($this->npassageiro > 0)){
+
+            return $this->npassageiro;
+
+        }else{
+
+            echo "Não há oque retornar ou é invalido";
+
+        }
+        
+
+
+
+       } //INT
+
+
+       public function veiculorodoviario(){
+
+        
+
+
+       }
+
+
 
 
     
@@ -52,7 +102,3 @@
 
 
     }
-
-
-
-?>
