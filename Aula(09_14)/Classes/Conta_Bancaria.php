c8067a1108f5cb9336ade7b6ec9c85810d0aeb17
Yan-Sena <25ygss@gmail.com>
2022-09-14 22:05:05 -0300

    Aula(09_14) - 20%

--- Aula(09_14)/Classes/Conta_Bancaria.php
+++ Aula(09_14)/Classes/Conta_Bancaria.php
@@ -10,19 +10,30 @@
         protected $saldo = 0.0;
 
 
+        function __construct($nro_conta, $dt_abertura, $dt_encerramento, $situacao, $senha, $saldo) {
+
+            $this->nro_conta = $nro_conta;
+            $this->dt_abertura = $dt_abertura;
+            $this->dt_encerramento = $dt_encerramento;
+            $this->situacao = $situacao;
+            $this->senha = $senha;
+            $this->saldo = $saldo;
+
+        }
+
 /*----------------------------------------------------------------------------------------------------------------------------------*/ 
-        public function abrir_Conta($numConta){
+    public function abrir_Conta($numConta){
 
-            if(is_int($numConta) && ($numConta == $this->nro_conta)){
-
-
-                echo "Bem Vindo. Iniciando conta ". $this->nro_conta . "<br>";
+        if(is_int($numConta) && ($numConta == $this->nro_conta)){
 
 
+            echo "Bem Vindo. Iniciando conta ". $this->nro_conta . "<br>";
 
-            }else{
 
-                echo "Essa conta não existe ou seu número está incorreto";
+
+        }else{
+
+            echo "Essa conta não existe ou seu número está incorreto";
 
             }
         }
@@ -30,14 +41,52 @@
 
     public function consultar_Conta($numConta){
 
-        
+        if(is_long($numConta) && ($numConta == $this->nro_conta)){
+
+            return true;
+           
+        }else{
+
+            return false;
+
+        }
+    }
+/*-------------------------------------------------------------------------------------------------------------------------------------*/
+
+    public function validar_senha($password){
+
+            if(is_int($password) && ($password == $this->senha)){
+
+
+                return true;
 
 
 
+            }else{
 
+                return false;
+
+            }
+        }
+
+/*-------------------------------------------------------------------------------------------------------------------------------------*/
+
+    public function saldo_Conta(){
+
+
+     return  $this->saldo;
 
 
     }
+
+/*-------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+
+    
 
 
 
